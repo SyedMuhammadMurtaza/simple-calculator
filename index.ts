f7b5cae3bf13765423095a72b404bd3d8dbf54b2
SyedMuhammadMurtaza <smmhj19@gmail.com>
2024-04-06 03:30:05 +0500

    First code for simple calculator using inquirer

---
+++ index.ts
@@ -0,0 +1,63 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let finalAnswer = 0;
+
+const answer = await inquirer.prompt([
+    {
+    message: "Enter first number",
+    type:"number",
+    name:"firstNumber"
+    },
+
+    {
+        message: "Enter second number",
+        type:"number",
+        name:"secondNumber"
+    },
+
+    {
+        message:"Select one of the operator to perform action",
+        type:"list",
+        name:"operator",
+        choices:["Addition", "Subtraction", "Multiplication", "Division"]
+    }    
+]);
+
+
+
+if (answer.operator === "Addition") {
+    finalAnswer = answer.firstNumber + answer.secondNumber;
+}
+
+else  if
+    (answer.operator === "Subtraction") {
+        finalAnswer = answer.firstNumber - answer.secondNumber;
+    }
+
+else if 
+    (answer.operator === "Multiplication") {
+        finalAnswer = answer.firstNumber * answer.secondNumber;
+    }
+
+else if 
+    (answer.operator === "Division") { 
+        finalAnswer = answer.firstNumber / answer.secondNumber;
+    }
+
+else {
+    console.log("Please select valid operator");
+}
+
+
+
+// console.log(answer);
+
+console.log(finalAnswer);
+
+
+
+
+
+ 
